4768a1638ec78ea9aa4f4ffd2b8e944d4f9cce09
QLIK0Y <qiu.li@scania.com>
2026-03-05 14:58:22 +0800

    colorful cube

--- fragShader-cube.glsl
+++ fragShader-cube.glsl
@@ -5,7 +5,10 @@ out vec4 color;
 uniform mat4 mv_matrix;
 uniform mat4 p_matrix;
 
+in vec4 varyingColor;
+
 void main(void)
 {
-	color = vec4(1.0, 0.0, 0.0, 1.0);
+	// color = vec4(0.75, 0.5, 0.25, 1.0);
+	color = varyingColor;
 }
